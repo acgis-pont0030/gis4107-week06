ba72a952c3b79a953b4d7f136e48a1f4f2689a01
Alex Pontefract <pont0030@algonquinlive.com>
2025-10-07 14:05:50 -0400

    added getxcodeposition

--- AlexP_BenP/doc_scanner.py
+++ AlexP_BenP/doc_scanner.py
@@ -5,5 +5,12 @@ def has_x_code(in_text):
     else:
         return False
 
+def get_x_code_position(in_text):
+    encode = 'Tx6op3'
+    if encode in in_text:
+        return in_text.find(encode)+1
+    else:
+        return -1
+
     
    
