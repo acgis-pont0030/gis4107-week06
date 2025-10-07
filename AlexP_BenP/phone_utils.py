a679ad97f70150f14d8ddca115bacf7358dd523a
Alex Pontefract <pont0030@algonquinlive.com>
2025-10-07 14:49:47 -0400

    fixed phone utils

--- AlexP_BenP/phone_utils.py
+++ AlexP_BenP/phone_utils.py
@@ -1,5 +1,9 @@
 def is_valid_phone_number(phone_number):
-    if ''.join(phone_number[i] for i in [0:2, 4:6, 8:11]).isdigit() and ''.join(phone_number[i] for i in [3, 7]) == '--':
-        return True
+    if ''.join(phone_number[i] for i in [3, 7]) == '--':
+        if ''.join((phone_number[i] for i in range(0,2))).isdigit() and ''.join((phone_number[i] for i in range(4,6))).isdigit() and ''.join((phone_number[i] for i in range(8,11))).isdigit():
+            return True
+        else:
+            return False
     else:
         return False
+    
