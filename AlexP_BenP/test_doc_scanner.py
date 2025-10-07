ec86da1117cce3cd3a29d8088024cd178dbdd718
Alex Pontefract <pont0030@algonquinlive.com>
2025-10-07 14:13:36 -0400

    added second test

--- AlexP_BenP/test_doc_scanner.py
+++ AlexP_BenP/test_doc_scanner.py
@@ -7,3 +7,10 @@ import doc_scanner as ds
     )
 def test_has_x_code(in_text, expected):
     assert ds.has_x_code(in_text) == expected
+
+@pytest.mark.parametrize('in_text, expected',
+    [('abcdefg', -1),
+     ('abcTx6op3defg', 4)]
+    )
+def test_get_x_code_position(in_text, expected):
+    assert ds.get_x_code_position(in_text) == expected
