61266aa3b2565861c3d559f57e507031301d72d6
Ben Pelkey <pelk0014@algonquinlive.com>
2025-10-07 14:33:44 -0400

    Added checks for num and dashes

--- AlexP_BenP/phone_utils.py
+++ AlexP_BenP/phone_utils.py
@@ -1,6 +1,5 @@
 def is_valid_phone_number(phone_number):
-    if phone_number[0:2,4:6,11].isdigit() and phone_number[3,7] == '-':
+    if ''.join(phone_number[i] for i in [0:2, 4:6, 8:11]).isdigit() and ''.join(phone_number[i] for i in [3, 7]) == '--':
         return True
     else:
         return False
-    
